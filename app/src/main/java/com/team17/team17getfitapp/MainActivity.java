6cf3a19540681e8981d128c0520b5c50cc5e8995
Steven Crosby <scrosby09@byu.edu>
2021-03-23 13:15:21 -0700

    Initial commit

---
+++ app/src/main/java/com/team17/team17getfitapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.team17.team17getfitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
