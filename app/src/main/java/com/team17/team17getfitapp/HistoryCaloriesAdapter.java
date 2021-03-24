dc038dc3a8076d8e7b175b97816dc5387bd8b7ea
Steven Crosby <65566479+scrosby09@users.noreply.github.com>
2021-03-23 20:14:46 -0700

    Updated Login Activity

---
+++ app/src/main/java/com/team17/team17getfitapp/HistoryCaloriesAdapter.java
@@ -0,0 +1,4 @@
+package com.team17.team17getfitapp;
+
+public class HistoryCaloriesAdapter {
+}
---
+++ app/src/main/java/com/team17/team17getfitapp/Login.java
@@ -0,0 +1,26 @@
+package com.team17.team17getfitapp;
+
+import android.view.View;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import com.team17.team17getfitapp.R;
+import com.team17.team17getfitapp.UserModel;
+import com.team17.team17getfitapp.SessionManager;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+}
