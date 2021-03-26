43d2b2957963aa63a6be5172e407677bc831d352
Steven Crosby <65566479+scrosby09@users.noreply.github.com>
2021-03-25 19:34:31 -0700

    updated UserHistoryActivity

--- app/src/main/java/com/team17/team17getfitapp/UserHistoryActivity.java
+++ app/src/main/java/com/team17/team17getfitapp/UserHistoryActivity.java
@@ -1,4 +1,67 @@
 package com.team17.team17getfitapp;
 
-public class UserHistoryActivity {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class UserHistoryActivity extends AppCompatActivity {
+    ArrayList<CaloriesModel> modelArrayList;
+    private DatabaseReference mFirebaseDatabase;
+    private CalorieHistoryAdapter adapter;
+    private ListView listView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        SessionManager sessionManager = new SessionManager(getApplicationContext());
+
+
+        FirebaseDatabase mFirebaseInstance = FirebaseDatabase.getInstance();
+        mFirebaseDatabase = mFirebaseInstance.getReference("UserCalories").child(sessionManager.getSharedPrefItem(SessionManager.KEY_ID));
+        listView = (ListView) findViewById(R.id.history_listView);
+        modelArrayList = new ArrayList<>();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                System.out.println("Initial Outlook " + dataSnapshot.getValue());
+
+                modelArrayList.clear();
+
+                // Iterating through the nodes
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+
+                    System.out.println(" Post Outlook " + postSnapshot.getValue());
+
+                    // Get the item
+                    CaloriesModel artist = postSnapshot.getValue(CaloriesModel.class);
+
+                    // Add item to the list
+                    modelArrayList.add(artist);
+                }
+                adapter = new CalorieHistoryAdapter(UserHistoryActivity.this, modelArrayList);
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
 }
--- app/src/main/java/com/team17/team17getfitapp/UserProfileActivity.java
+++ app/src/main/java/com/team17/team17getfitapp/UserProfileActivity.java
@@ -1,4 +1,14 @@
 package com.team17.team17getfitapp;
 
-public class UserProfileActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userprofile);
+
+    }
 }
